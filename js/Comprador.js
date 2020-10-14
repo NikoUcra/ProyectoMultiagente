0984054ca1ecd4e71c8426f397a8488de38ae6fd
Mykola <nikoucra@gmail.com>
2020-10-14 13:47:02 +0200

    Creado el constructor del Agente Comprador

---
+++ js/Comprador.js
@@ -0,0 +1,12 @@
+class Comprador {
+
+    constructor(ip,port,ipmonitor,portmonitor){
+        this.ip=ip;
+        this.port=port;
+        this.ipmonitor=ipmonitor;
+        this.portmonitor=portmonitor;
+        this.id=null;
+        this.listaCompra = [];
+        this.listaTiendas = [];
+    }
+}  
