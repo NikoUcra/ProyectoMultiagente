0b9acb037df0afbeaee13647b9b2c189c8287d7c
NikoUcra <34657286+NikoUcra@users.noreply.github.com>
2020-10-27 16:24:49 +0100

    Update Comprador.js

--- js/Comprador.js
+++ js/Comprador.js
@@ -1,12 +1,36 @@
-class Comprador {
+class Comprador{
 
-    constructor(ip,port,ipmonitor,portmonitor){
+    constructor(ip,puerto,ipMonitor,puertoMonitor){
         this.ip=ip;
-        this.port=port;
-        this.ipmonitor=ipmonitor;
-        this.portmonitor=portmonitor;
+        this.ipMonitor=ipMonitor;
+        this.puertoMonitor=puertoMonitor;
         this.id=null;
         this.listaCompra = [];
         this.listaTiendas = [];
     }
-}  
+
+    run(){
+        Agente.envios(this.ipMonitor,this.puertoMonitor,GestorMensajes.pedirID(),GestorMensajes.procesaID(),this);
+        for(tienda in listaTiendas){
+            if(this.listaCompra != []){
+            this.entrarTienda(tienda);
+            this.pideTienda(tienda);
+            this.salirTienda(tienda);
+            }else{
+                this.desconectar();
+            }
+        }
+
+    }
+
+    entrarTienda(tienda){
+        Agente.envios(tienda.ip,tienda.puerto,GestorMensajes.comprar(),GestorMensajes.procesaCompra(),this);
+    }
+    salirTienda(tienda){
+        Agente.envios(tienda.ip,tienda.puerto,GestorMensajes.salir(),GestorMensajes.procesaSalida(),this);
+    };
+    desconectar(){
+        Agente.envios(this.ipMonitor,this.puertoMonitor,GestorMensajes.desconectar(),GestorMensajes.procesaDesconecta(),this);
+    }
+
+}
